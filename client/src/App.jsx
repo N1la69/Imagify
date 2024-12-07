2c638be1ce4517d0e137bb49d70f555753b656b8
Nilanjan Saha <nilanjan.s.1920@gmail.com>
2024-12-07 12:54:18 +0530

    initial

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import BuyCredit from "./pages/BuyCredit";
+import Home from "./pages/Home";
+import Result from "./pages/Result";
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+      <Result />
+      <BuyCredit />
+    </div>
+  );
+};
+
+export default App;
