88eb9251f48ff667a76a96107bbe64fd4dc2e230
Nilanjan Saha <nilanjan.s.1920@gmail.com>
2024-12-09 19:53:49 +0530

    UserModel, Controller and Route

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 import express from "express";
-import dotenv from "dotenv";
+import "dotenv/config";
 import cors from "cors";
+import connectDB from "./config/mongodb.js";
+import userRouter from "./routes/userRoute.js";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+await connectDB();
 
 app.get("/", (req, res) => res.send("API Working"));
+app.use("/api/user", userRouter);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
