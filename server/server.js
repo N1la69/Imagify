2949b113793e4ada0caad43da83d5467c211e43a
Nilanjan Saha <nilanjan.s.1920@gmail.com>
2024-12-10 19:41:04 +0530

    Image generator api

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import cors from "cors";
 import connectDB from "./config/mongodb.js";
 import userRouter from "./routes/userRoute.js";
+import imageRouter from "./routes/imageRoutes.js";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -13,5 +14,6 @@ await connectDB();
 
 app.get("/", (req, res) => res.send("API Working"));
 app.use("/api/user", userRouter);
+app.use("/api/image", imageRouter);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
