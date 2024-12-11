4f852ef264ddc752579c27ab8fddb27955131000
Nilanjan Saha <nilanjan.s.1920@gmail.com>
2024-12-11 21:41:20 +0530

    payment

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,8 @@
 import userModel from "../models/userModel.js";
+import transactionModel from "../models/transactionModel.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import razorpay from "razorpay";
 
 export const registerUser = async (req, res) => {
   try {
@@ -91,6 +93,89 @@ export const userCredits = async (req, res) => {
   } catch (error) {
     console.log(error);
     return res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+const razorpayInstance = new razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+export const paymentRazorpay = async (req, res) => {
+  try {
+    const { userId, planId } = req.body;
+    const userData = await userModel.findById(userId);
+
+    if (!userId || !planId) {
+      return res.json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+
+    let credits, plan, amount, date;
+
+    switch (planId) {
+      case "Basic":
+        plan = "Basic";
+        credits = 100;
+        amount = 10;
+        break;
+      case "Advanced":
+        plan = "Advanced";
+        credits = 500;
+        amount = 50;
+        break;
+      case "Business":
+        plan = "Business";
+        credits = 5000;
+        amount = 250;
+        break;
+
+      default:
+        return res.json({
+          success: false,
+          message: "Invalid plan",
+        });
+    }
+    date = Date.now();
+
+    const transactionData = {
+      userId,
+      plan,
+      credits,
+      amount,
+      date,
+    };
+
+    const newTransaction = await transactionModel.create(transactionData);
+
+    const options = {
+      amount: amount * 100,
+      currency: process.env.CURRENCY,
+      receipt: newTransaction._id,
+    };
+
+    await razorpayInstance.orders.create(options, (error, order) => {
+      if (error) {
+        console.log(error);
+        return res.json({
+          success: false,
+          message: error.message,
+        });
+      }
+
+      res.json({
+        success: true,
+        order,
+      });
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
       success: false,
       message: error.message,
     });
